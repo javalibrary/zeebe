67d294f3e434934bfb390cc487a5e09e9350f262
Sebastian Menski <sebastian.menski@camunda.com>
2019-04-30 14:39:21 +0200

    chore(ci): use different local maven repository for integration tests

--- .ci/scripts/distribution/build-java.sh
+++ .ci/scripts/distribution/build-java.sh
@@ -3,4 +3,7 @@
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
-su jenkins -c "mvn -B -T2 -s settings.xml -DskipTests clean com.mycila:license-maven-plugin:check com.coveo:fmt-maven-plugin:check install -Pspotbugs"
+su jenkins -c "mvn -B -T2 -s settings.xml -Dmaven.repo.local=/tmp/maven -DskipTests clean com.mycila:license-maven-plugin:check com.coveo:fmt-maven-plugin:check install -Pspotbugs"
+
+# duplicate maven repository for qa tests
+su jenkins -c "cp -R /tmp/maven /tmp/maven-it"
